e362c372220d8768dec6175851d3120efa980706
Salman Ahmed <ahmedsalman392@gmail.com>
2019-11-20 20:21:38 +0530

    added social web app

---
+++ social_web/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+// import Home from './screens/home';
+import AddUser from './screens/adduser';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Welcome to Social </h1>
+      <AddUser />
+      {/* <Home /> */}
+    </div>
+  );
+}
+
+export default App;
